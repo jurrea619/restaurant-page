65276beb5ead8248976c670e779549d27fe6c193
Joshua Urrea <jurrea619@gmail.com>
2026-08-08 21:41:32 -0700

    Finish home page module import

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 import "./styles.css";
-// grab content div for placing new elements
-const contentDiv = document.getElementById('content');
+import { loadHomePage } from "./home.js";
 
-// INITIAL PAGE LOAD
-
-// create div
-const containerDiv = document.createElement('div');
-// add class
-containerDiv.classList.add('main-container');
-
-// create heading current page (home, menu, about)
-const pageHeader = document.createElement('h1');
-pageHeader.textContent = "Coffee Corner";
-// create span for page slogan
-const mySpan = document.createElement('span');
-mySpan.textContent = "For those who love coffee AND corners!";
-
-// add to container
-containerDiv.appendChild(pageHeader);
-containerDiv.appendChild(mySpan);
-// add to content
-contentDiv.appendChild(containerDiv);
+loadHomePage();
