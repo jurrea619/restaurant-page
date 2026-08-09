8a9cfe19dda749bd1c75b7556a1d0e1cbcabf2f8
Joshua Urrea <jurrea619@gmail.com>
2026-08-08 21:13:41 -0700

    Refactor index.js to handle loading Home Page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
+// grab content div for placing new elements
+const contentDiv = document.getElementById('content');
 
-console.log("Here we go!");
+// INITIAL PAGE LOAD
+
+// create div
+const containerDiv = document.createElement('div');
+// add class
+containerDiv.classList.add('main-container');
+
+// create heading current page (home, menu, about)
+const pageHeader = document.createElement('h1');
+pageHeader.textContent = "Coffee Corner";
+// create span for page slogan
+const mySpan = document.createElement('span');
+mySpan.textContent = "For those who love coffee AND corners!";
+
+// add to container
+containerDiv.appendChild(pageHeader);
+containerDiv.appendChild(mySpan);
+// add to content
+contentDiv.appendChild(containerDiv);
